8e4345613257c61a484f3f5bdd0585f521bcbcc1
D1ffic00lt <dm.filinov@gmail.com>
2022-02-22 18:24:09 +0700

    README changed

--- ModelLibrary/predict.py
+++ ModelLibrary/predict.py
@@ -1,16 +1,44 @@
-# import pickle
+import string
 
-from langdetect import detect
+from langdetect import detect, LangDetectException
+from nltk import word_tokenize, SnowballStemmer
+from nltk.corpus import stopwords
 
-from models import EnglishTokinizer, RussianTokinizer
 
-# with open("models/EnglishModel.bf", "rb") as EnglishModel, \
-#         open("models/RussianModel.bf", "rb") as RussianModel:
-#     models_ = [pickle.load(RussianModel), pickle.load(EnglishModel)]
-#
-# with open("models/RussianVectorizer.bf", "rb") as RussianVectorizer, \
-#         open("models/EnglishVectorizer.bf", "rb") as EnglishVectorizer:
-#     vectorizers_ = [pickle.load(RussianVectorizer), pickle.load(EnglishVectorizer)]
+RussianSnowball_ = SnowballStemmer(language="russian")
+EnglishSnowball_ = SnowballStemmer(language="english")
+
+
+def RussianTokinizer(sentence: str):
+    sentence = sentence.strip().lower()
+
+    try:
+        if detect(sentence) == "ru":
+            sentence.replace("e", "е").replace("a", "а").replace("c", "с").replace("p", "р").replace("o", "о").replace(
+                "M", "м").replace("H", "н").replace("B", "в")
+            sentence.replace("не ", "не").replace("no ", "no")
+    except LangDetectException:
+        pass
+    sentence.replace("\t", " ")
+    for i in string.punctuation:
+        if i in sentence and i != " ":
+            sentence = sentence.replace(i, '')
+    tokens = word_tokenize(sentence, language="russian")
+    tokens = [i for i in tokens if i not in stopwords.words("russian")]
+    tokens = [RussianSnowball_.stem(i) for i in tokens]
+    return " ".join(tokens)
+
+
+def EnglishTokinizer(sentence: str):
+    sentence = sentence.strip().lower()
+    sentence.replace("\t", " ")
+    for i in string.punctuation:
+        if i in sentence and i != " ":
+            sentence = sentence.replace(i, '')
+    tokens = word_tokenize(sentence, language="english")
+    tokens = [i for i in tokens if i not in stopwords.words("english")]
+    tokens = [EnglishSnowball_.stem(i) for i in tokens]
+    return " ".join(tokens)
 
 
 def GetToxicity(string_: str = None, models: list = None, vectorizers: list = None):
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # anonymous-bot-with-ML
+___
+```Python
+from ModelLibrary.predict import GetToxicity
+import pickle
+
+with open("ModelLibrary/models/EnglishModel.bf", "rb") as EnglishModel, \
+        open("ModelLibrary/models/RussianModel.bf", "rb") as RussianModel:
+    models_ = [pickle.load(RussianModel), pickle.load(EnglishModel)]
+
+with open("ModelLibrary/models/RussianVectorizer.bf", "rb") as RussianVectorizer, \
+        open("ModelLibrary/models/EnglishVectorizer.bf", "rb") as EnglishVectorizer:
+    vectorizers_ = [pickle.load(RussianVectorizer), pickle.load(EnglishVectorizer)]
+
+print(GetToxicity("ПРИВЕТ КАК ДЕЛА&", models=models_, vectorizers=vectorizers_))
+```
