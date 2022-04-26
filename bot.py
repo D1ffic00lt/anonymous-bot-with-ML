618e1edcbc855a1b3ed827bb58f0e187acdbafe5
Sergey-piksel <61351134+Sergey-piksel@users.noreply.github.com>
2022-04-26 23:09:29 +0700

    Пиу пиу
    пау

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ from database import Database
 import pickle                                                                        # Loading library for reading models
 from ModelLibrary.predict import GetToxicity                                    # Loading the training program
 import nltk
+from datetime import datetime
+import json
+import os
+from langdetect.lang_detect_exception import LangDetectException
 print("\n" + "Program started" + "\n")
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -27,6 +31,29 @@ db = Database('db.db')
 client = telebot.TeleBot(configure.config['token'])
 print("Bot connected")
 db.restart()
+
+
+month = int(datetime.today().strftime('%m'))
+day = int(datetime.today().strftime('%d'))
+if not os.path.exists("last_save.json"):
+    with open('last_save.json', 'w+') as outfile:
+        json.dump({
+            "day": day,
+            "month": month,
+        }, outfile)
+else:
+    with open("last_save.json", "r") as f:
+        data = json.loads(f.read())
+        day_json = data["day"]
+        month_json = data["month"]
+    if (day > day_json and month == month_json) or \
+            (day < day_json and month > month_json) or \
+            (month == 1 and day == 1 and day < day_json):
+        with open('last_save.json', 'w+') as outfile:
+            json.dump({
+                "day": day,
+                "month": month,
+            }, outfile)
 
 
 rating = {}
@@ -63,6 +90,16 @@ def start(message):
     client.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Добро пожаловать в анонимный чат!'
                                          'Укажите ваш пол. ', reply_markup=markup)
 
+@client.message_handler(commands=['help'])
+def help(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Поиск собеседника')
+    item2 = types.KeyboardButton('Меню')
+    markup.add(item1, item2)
+
+    client.send_message(message.chat.id, f'Привет, {message.from_user.first_name}! Добро пожаловать в анонимный чат!'
+                                         'Укажите ваш пол. ', reply_markup=markup)
+
 
 @client.message_handler(commands=['menu'])
 def menu(message):
@@ -70,28 +107,97 @@ def menu(message):
     item1 = types.KeyboardButton('Поиск собеседника')
     markup.add(item1)
 
-    client.send_message(message.chat.id, 'Меню', reply_markup=markup)
+    keyboard = types.InlineKeyboardMarkup()
+    key1 = types.InlineKeyboardButton(text='Следующий собеседник', callback_data='search', row_width=3)
+    keyboard.add(key1)
+    client.send_message(message.chat.id, '···МЕНЮ···\n'
+                                         'Ваш профиль (виден только Вам):\n'
+                                         f'Имя: {message.from_user.first_name}\n'
+                                         f'Пол: {"Мужской" if db.get_gender(message.chat.id) == "male" else "Женский"}\n'
+                                         f'Рейтинг: {db.get_rating(message.chat.id)[0]:.4f}\n'
+                                         f'/help - помощь в использовании бота', reply_markup=keyboard)
+
+    @client.callback_query_handler(func=lambda call: True)
+    def callback_work(call):
+        if call.data == 'search':
+            if not db.is_register(message.chat.id) and message.text != '/start':
+                client.send_message(message.chat.id, 'Вы не зарегистрировались! Напишите /start для регистрации!')
+            else:
+                markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                item1 = types.KeyboardButton('Парень')
+                item2 = types.KeyboardButton('Девушка')
+                item3 = types.KeyboardButton('Любое')
+                markup2.add(item1, item2, item3)
+
+                client.send_message(message.chat.id, 'Кого искать?', reply_markup=markup2)
+        client.answer_callback_query(callback_query_id=call.id) # должно убрать часы на кнопке
 
 
 @client.message_handler(commands=['stop'])
 def stop(message):
     chat_info = db.get_active_chat(message.chat.id)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton('Следующий диалог')
-    item2 = types.KeyboardButton('/menu')
-    markup.add(item1, item2)
+
+    keyboard = types.InlineKeyboardMarkup()
+    key1 = types.InlineKeyboardButton(text='Спам', callback_data='spam', row_width=3)
+    key2 = types.InlineKeyboardButton(text='Обман', callback_data='deceit', row_width=3)
+    key3 = types.InlineKeyboardButton(text='Продажа', callback_data='sale', row_width=3)
+    key4 = types.InlineKeyboardButton(text='18+', callback_data='18+', row_width=3)
+    key5 = types.InlineKeyboardButton(text='Следующий собеседник', callback_data='search', row_width=1)
+    keyboard.add(key1)
+    keyboard.add(key2)
+    keyboard.add(key3)
+    keyboard.add(key4)
+    keyboard.add(key5)
+
+    @client.callback_query_handler(func=lambda call: True)
+    def callback_worker(call):
+        print(call)
+        if call.data == 'spam':
+            markup = types.InlineKeyboardMarkup()
+            item = types.InlineKeyboardButton(text='Поиск собеседника', callback_data='search')
+            markup.add(item)
+            client.edit_message_text(chat_id=call.from_user.id, message_id=call.message.id, text="Жалоба отправлена", reply_markup=markup)
+        elif call.data == 'deceit':
+            markup = types.InlineKeyboardMarkup()
+            item = types.InlineKeyboardButton(text='Поиск собеседника', callback_data='search')
+            markup.add(item)
+            client.edit_message_text(chat_id=call.from_user.id, message_id=call.message.id, text="Жалоба отправлена", reply_markup=markup)
+        elif call.data == 'sale':
+            markup = types.InlineKeyboardMarkup()
+            item = types.InlineKeyboardButton(text='Поиск собеседника', callback_data='search')
+            markup.add(item)
+            client.edit_message_text(chat_id=call.from_user.id, message_id=call.message.id, text="Жалоба отправлена", reply_markup=markup)
+        elif call.data == '18+':
+            markup = types.InlineKeyboardMarkup()
+            item = types.InlineKeyboardButton(text='Поиск собеседника', callback_data='search')
+            markup.add(item)
+            client.edit_message_text(chat_id=call.from_user.id, message_id=call.message.id, text="Жалоба отправлена", reply_markup=markup)
+        elif call.data == 'search':
+            if not db.is_register(message.chat.id) and message.text != '/start':
+                client.send_message(message.chat.id, 'Вы не зарегистрировались! Напишите /start для регистрации!')
+            else:
+                markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                item1 = types.KeyboardButton('Парень')
+                item2 = types.KeyboardButton('Девушка')
+                item3 = types.KeyboardButton('Любое')
+                markup2.add(item1, item2, item3)
+
+                client.send_message(message.chat.id, 'Кого искать?', reply_markup=markup2)
+        client.answer_callback_query(callback_query_id=call.id) # должно убрать часы на кнопке
+
+
     if chat_info != False:
         db.delete_chat(chat_info[0])
 
-        client.send_message(chat_info[1], 'Собеседник покинул чат', reply_markup=markup)
-        client.send_message(message.chat.id, 'Вы вышли из чата', reply_markup=markup)
+        client.send_message(chat_info[1], 'Собеседник покинул чат\n\nВы можете оставить жалобу на вашего собеседника:', reply_markup=keyboard)
+        client.send_message(message.chat.id, 'Вы вышли из чата\n\nВы можете оставить жалобу на вашего собеседника:', reply_markup=keyboard)
 
         db.change_rating(message.chat.id, rating[str(message.chat.id)])
         db.change_rating(chat_info[1], rating[str(chat_info[1])])
         del rating[str(message.chat.id)]
         del rating[str(chat_info[1])]
     else:
-        client.send_message(message.chat.id, 'Вы не начали чат', reply_markup=markup)
+        client.send_message(message.chat.id, 'Вы не начали диалог!', reply_markup=keyboard)
 
 
 @client.message_handler(content_types=['text'])
@@ -111,7 +217,9 @@ def bot_message(message):
 
         elif message.text == 'Остановить поиск':
             db.delete_queue(message.chat.id)
-            client.send_message(message.chat.id, 'Поиск остановлен, напишите /menu', reply_markup=main_menu())
+            client.send_message(message.chat.id, 'Поиск остановлен:\n'
+                                                 '· Напишите /menu для выхода в меню\n'
+                                                 '· Нажмите на кнопку "Поиск собеседника", чтобы найти следующий диалог', reply_markup=main_menu())
 
         elif message.text == 'Парень':
             user_info = db.get_gender_chat('male')
@@ -168,7 +276,7 @@ def bot_message(message):
                 else:
                     client.send_message(message.chat.id, 'В вашем аккаунте не указан username')
             else:
-                client.send_message(message.chat.id, 'Вы не начали диалог')
+                client.send_message(message.chat.id, 'Вы не начали диалог!')
 
         elif message.text == 'Я парень':
             if db.set_gender(message.chat.id, 'male'):
@@ -186,14 +294,17 @@ def bot_message(message):
             if db.get_active_chat(message.chat.id) is not False:
                 chat_info = db.get_active_chat(message.chat.id)
                 client.send_message(chat_info[1], message.text)
-                toxicity = GetToxicity(message.text, models=models_, vectorizers=vectorizers_)[1]
-                toxicity_score = 70 - (toxicity * 100) if toxicity < 0.6 else 40 - (toxicity * 100)
-                print(rating[str(message.chat.id)])
-                print(message.chat.id)
-                rating[str(message.chat.id)] += toxicity_score
-                print(rating)
+                try:
+                    toxicity = GetToxicity(message.text, models=models_, vectorizers=vectorizers_)[1]
+                    toxicity_score = 70 - (toxicity * 100) if toxicity < 0.6 else 40 - (toxicity * 100)
+                    print(rating[str(message.chat.id)])
+                    print(message.chat.id)
+                    rating[str(message.chat.id)] += toxicity_score
+                    print(rating)
+                except LangDetectException:
+                    pass
             else:
-                client.send_message(message.chat.id, 'Вы не начали диалог')
+                client.send_message(message.chat.id, 'Вы не начали диалог!')
 
 
 @client.message_handler(content_types=['sticker'])
@@ -203,7 +314,7 @@ def bot_stickers(message):
         if chat_info is not False:
             client.send_sticker(chat_info[1], message.sticker.file_id)
         else:
-            client.send_message(message.chat.id, 'Вы не начали диалог')
+            client.send_message(message.chat.id, 'Вы не начали диалог!')
 
 
 @client.message_handler(content_types=['voice'])
@@ -213,7 +324,7 @@ def bot_voice(message):
         if chat_info is not False:
             client.send_voice(chat_info[1], message.voice.file_id)
         else:
-            client.send_message(message.chat.id, 'Вы не начали диалог')
+            client.send_message(message.chat.id, 'Вы не начали диалог!')
 
 
 @client.message_handler(content_types=['document'])
@@ -223,7 +334,7 @@ def bot_document(message):
         if chat_info is not False:
             client.send_document(chat_info[1], message.document.file_id)
         else:
-            client.send_message(message.chat.id, 'Вы не начали диалог')
+            client.send_message(message.chat.id, 'Вы не начали диалог!')
 
 
 @client.message_handler(content_types=['video'])
@@ -233,7 +344,7 @@ def bot_video(message):
         if chat_info is not False:
             client.send_video(chat_info[1], message.video.file_id)
         else:
-            client.send_message(message.chat.id, 'Вы не начали диалог')
+            client.send_message(message.chat.id, 'Вы не начали диалог!')
 
 
 @client.message_handler(content_types=['photo'])
@@ -245,11 +356,7 @@ def bot_photo(message):
                 print(message.photo[0])
                 client.send_photo(chat_info[1], message.photo[0].file_id)
             else:
-                client.send_message(message.chat.id, 'Вы не начали диалог')
+                client.send_message(message.chat.id, 'Вы не начали диалог!')
 
 
 client.polling(non_stop=True, interval=0)
-
-
-#  toxicity = GetToxicity(message, models=models_, vectorizers=vectorizers_))[1]
-# toxicity = 70 - (toxicity * 100) if toxicity < 0.6 else 40 - (toxicity * 100))
